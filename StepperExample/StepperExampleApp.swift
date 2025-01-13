9e2a0cf45fa766dc6103b89907ea9a7ae963e03c
Joseph Ji <jji@lcs.on.ca>
2025-01-13 08:54:25 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Xinchen Ji on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
